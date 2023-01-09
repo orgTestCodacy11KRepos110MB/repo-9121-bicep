31fbca159fa7a9fb27787be7adbe280448389622
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-01-09 04:42:20 -0500

    Fix lint issue

--- src/vscode-bicep/src/test/unit/logger.test.ts
+++ src/vscode-bicep/src/test/unit/logger.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-jest.mock("winston", () => ({
+jest.mock<object>("winston", () => ({
   createLogger: () => mockWinstonLogger,
   format: {
     combine: jest.fn(),
